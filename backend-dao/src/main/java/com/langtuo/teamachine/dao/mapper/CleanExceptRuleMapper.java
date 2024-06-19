3836affbcade879a153eecb538afd94fe5b0b665
米亚 <futuretask@126.com>
2024-06-19 10:27:57 +0800

    add func

---
+++ backend-dao/src/main/java/com/langtuo/teamachine/dao/mapper/CleanExceptRuleMapper.java
@@ -0,0 +1,53 @@
+package com.langtuo.teamachine.dao.mapper;
+
+import com.langtuo.teamachine.dao.annotation.GxTableShard;
+import com.langtuo.teamachine.dao.annotation.MySQLScan;
+import com.langtuo.teamachine.dao.po.CleanExceptRulePO;
+import com.langtuo.teamachine.dao.po.CleanRulePO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@MySQLScan
+@Repository
+public interface CleanExceptRuleMapper {
+    /**
+     *
+     * @param tenantCode
+     * @param cleanRuleCode
+     * @return
+     */
+    CleanExceptRulePO selectOne(@Param("tenantCode") String tenantCode, @Param("cleanRuleCode") String cleanRuleCode, @Param("exceptToppingCode") String exceptToppingCode);
+
+    /**
+     *
+     * @return
+     */
+    List<CleanExceptRulePO> selectList();
+
+    /**
+     *
+     * @param cleanExceptRulePO
+     * @return
+     */
+    @GxTableShard(tableShardOpen = true, shardName = "table_new", columns = "version", defaultName = "table")
+    int insert(CleanExceptRulePO cleanExceptRulePO);
+
+    /**
+     *
+     * @param cleanExceptRulePO
+     * @return
+     */
+    int update(CleanExceptRulePO cleanExceptRulePO);
+
+    /**
+     *
+     * @param tenantCode
+     * @param cleanRuleCode
+     * @return
+     */
+    int delete(@Param("tenantCode") String tenantCode, @Param("cleanRuleCode") String cleanRuleCode, @Param("exceptToppingCode") String exceptToppingCode);
+}
---
+++ backend-dao/src/main/java/com/langtuo/teamachine/dao/testor/CleanExceptRulePOTestor.java
@@ -0,0 +1,95 @@
+package com.langtuo.teamachine.dao.testor;
+
+import com.langtuo.teamachine.dao.helper.SqlSessionFactoryHelper;
+import com.langtuo.teamachine.dao.mapper.CleanExceptRuleMapper;
+import com.langtuo.teamachine.dao.po.CleanExceptRulePO;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class CleanExceptRulePOTestor {
+    public static void main(String args[]) {
+//        insert();
+//        select();
+        delete();
+    }
+
+    public static void insert() {
+        SqlSession sqlSession = SqlSessionFactoryHelper.getSqlSession();
+        CleanExceptRuleMapper mapper = sqlSession.getMapper(CleanExceptRuleMapper.class);
+
+        CleanExceptRulePO po = null;
+
+        po = new CleanExceptRulePO();
+        po.setTenantCode("tenant_001");
+        po.setCleanRuleCode("clean_rule_001");
+        po.setExceptToppingCode("except_001");
+        mapper.insert(po);
+
+        po = new CleanExceptRulePO();
+        po.setTenantCode("tenant_002");
+        po.setCleanRuleCode("clean_rule_002");
+        po.setExceptToppingCode("except_002");
+        mapper.insert(po);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    public static void select() {
+        SqlSession sqlSession = SqlSessionFactoryHelper.getSqlSession();
+        CleanExceptRuleMapper mapper = sqlSession.getMapper(CleanExceptRuleMapper.class);
+
+        List<CleanExceptRulePO> list = mapper.selectList();
+        for (CleanExceptRulePO po : list) {
+            System.out.printf("$$$$$$$$$$ list->po: %s\n", po);
+        }
+
+        CleanExceptRulePO po = mapper.selectOne("tenant_002", "clean_rule_002", "except_002");
+        System.out.printf("$$$$$$$$$$ po: %s\n", po);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    public static void delete() {
+        SqlSession sqlSession = SqlSessionFactoryHelper.getSqlSession();
+        CleanExceptRuleMapper mapper = sqlSession.getMapper(CleanExceptRuleMapper.class);
+
+        mapper.delete("tenant_001", "clean_rule_001", "except_001");
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+//    public static void testBySpring() {
+//        ApplicationContext context = startBySpring();
+//
+//        MachineMapper mapper =
+//                (MachineMapper) context.getBean(MachineMapper.class);
+//        MachinePojo machinePojo = mapper.getOne("machine_001");
+//        System.out.printf("DaoDemo#testBySpring hotelGuestPojo=%s\n", machinePojo);
+//
+//        stopBySpring(context);
+//    }
+//
+//    public static ApplicationContext startBySpring() {
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-config.xml");
+//        if (context == null) {
+//            throw new RuntimeException("DaoDemo#startBySpring初始化Spring失败");
+//        }
+//        return context;
+//    }
+//
+//    public static void stopBySpring(ApplicationContext context) {
+//        if (context == null) {
+//            return;
+//        }
+//        if (context instanceof ClassPathXmlApplicationContext) {
+//            ((ClassPathXmlApplicationContext) context).close();
+//        } else {
+//            System.out.println("DaoDemo#stopBySpring上下文参数异常");
+//        }
+//    }
+}
