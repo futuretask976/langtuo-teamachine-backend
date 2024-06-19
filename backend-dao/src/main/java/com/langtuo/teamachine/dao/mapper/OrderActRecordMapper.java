852fdc5b026683b5f948c8d725c37fc38943b6bf
米亚 <futuretask@126.com>
2024-06-19 14:21:28 +0800

    add func

---
+++ backend-dao/src/main/java/com/langtuo/teamachine/dao/mapper/OrderActRecordMapper.java
@@ -0,0 +1,52 @@
+package com.langtuo.teamachine.dao.mapper;
+
+import com.langtuo.teamachine.dao.annotation.GxTableShard;
+import com.langtuo.teamachine.dao.annotation.MySQLScan;
+import com.langtuo.teamachine.dao.po.CleanActRecordPO;
+import com.langtuo.teamachine.dao.po.OrderActRecordPO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@MySQLScan
+@Repository
+public interface OrderActRecordMapper {
+    /**
+     *
+     * @param tenantCode
+     * @param machineCode
+     * @param shopCode
+     * @param orderGmtCreated
+     * @return
+     */
+    OrderActRecordPO selectOne(@Param("tenantCode") String tenantCode, @Param("machineCode") String machineCode,
+            @Param("shopCode") String shopCode, @Param("orderGmtCreated") String orderGmtCreated);
+
+    /**
+     *
+     * @return
+     */
+    List<OrderActRecordPO> selectList();
+
+    /**
+     *
+     * @param orderActRecordPO
+     * @return
+     */
+    @GxTableShard(tableShardOpen = true, shardName = "table_new", columns = "version", defaultName = "table")
+    int insert(OrderActRecordPO orderActRecordPO);
+
+    /**
+     *
+     * @param tenantCode
+     * @param machineCode
+     * @param shopCode
+     * @param orderGmtCreated
+     * @return
+     */
+    int delete(@Param("tenantCode") String tenantCode, @Param("machineCode") String machineCode,
+               @Param("shopCode") String shopCode, @Param("orderGmtCreated") String orderGmtCreated);
+}
