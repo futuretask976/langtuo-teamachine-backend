358ab4e5c93f4b761a9f6042b85206a0bdd5329b
米亚 <futuretask@126.com>
2024-06-19 16:59:19 +0800

    add func

--- backend-dao/src/main/java/com/langtuo/teamachine/dao/testor/MachineModelPOTestor.java
+++ backend-dao/src/main/java/com/langtuo/teamachine/dao/testor/MachineModelPOTestor.java
@@ -24,7 +24,6 @@ public class MachineModelPOTestor {
         po.setEnableFlowAll(0);
         po.setExtraInfo(new HashMap<String, String>(){{
             put("k1", "v1");
-            put("k2", "v2");
         }});
         mapper.insert(po);
 
@@ -32,8 +31,71 @@ public class MachineModelPOTestor {
         po.setModelCode("model_002");
         po.setEnableFlowAll(1);
         po.setExtraInfo(new HashMap<String, String>(){{
-            put("k11", "v11");
-            put("k22", "v22");
+            put("k2", "v2");
+        }});
+        mapper.insert(po);
+
+        po = new MachineModelPO();
+        po.setModelCode("model_003");
+        po.setEnableFlowAll(1);
+        po.setExtraInfo(new HashMap<String, String>(){{
+            put("k3", "v3");
+        }});
+        mapper.insert(po);
+
+        po = new MachineModelPO();
+        po.setModelCode("model_004");
+        po.setEnableFlowAll(1);
+        po.setExtraInfo(new HashMap<String, String>(){{
+            put("k4", "v4");
+        }});
+        mapper.insert(po);
+
+        po = new MachineModelPO();
+        po.setModelCode("model_005");
+        po.setEnableFlowAll(1);
+        po.setExtraInfo(new HashMap<String, String>(){{
+            put("k5", "v5");
+        }});
+        mapper.insert(po);
+
+        po = new MachineModelPO();
+        po.setModelCode("model_006");
+        po.setEnableFlowAll(1);
+        po.setExtraInfo(new HashMap<String, String>(){{
+            put("k6", "v6");
+        }});
+        mapper.insert(po);
+
+        po = new MachineModelPO();
+        po.setModelCode("model_007");
+        po.setEnableFlowAll(1);
+        po.setExtraInfo(new HashMap<String, String>(){{
+            put("k7", "v7");
+        }});
+        mapper.insert(po);
+
+        po = new MachineModelPO();
+        po.setModelCode("model_008");
+        po.setEnableFlowAll(1);
+        po.setExtraInfo(new HashMap<String, String>(){{
+            put("k8", "v8");
+        }});
+        mapper.insert(po);
+
+        po = new MachineModelPO();
+        po.setModelCode("model_009");
+        po.setEnableFlowAll(1);
+        po.setExtraInfo(new HashMap<String, String>(){{
+            put("k9", "v9");
+        }});
+        mapper.insert(po);
+
+        po = new MachineModelPO();
+        po.setModelCode("model_010");
+        po.setEnableFlowAll(1);
+        po.setExtraInfo(new HashMap<String, String>(){{
+            put("k10", "v10");
         }});
         mapper.insert(po);
 
