20ae89258b117f58d2636dc056e5e11e87011893
米亚 <futuretask@126.com>
2024-07-05 08:56:52 +0800

    add func

--- backend-biz/src/main/java/com/langtuo/teamachine/biz/service/util/DeployUtils.java
+++ backend-biz/src/main/java/com/langtuo/teamachine/biz/service/util/DeployUtils.java
@@ -3,15 +3,16 @@ package com.langtuo.teamachine.biz.service.util;
 import java.security.SecureRandom;
 
 public class DeployUtils {
+    private static final String CHAR_SET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
     public static String genRandomStr(int length) {
-        String charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         SecureRandom random = new SecureRandom();
         random.setSeed(System.currentTimeMillis());
 
         StringBuilder sb = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
-            int randomIndex = random.nextInt(charSet.length());
-            char randomChar = charSet.charAt(randomIndex);
+            int randomIndex = random.nextInt(CHAR_SET.length());
+            char randomChar = CHAR_SET.charAt(randomIndex);
             sb.append(randomChar);
         }
         return sb.toString();
