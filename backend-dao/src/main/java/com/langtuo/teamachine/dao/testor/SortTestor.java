acee972e334c6888857811eb75aa716363fbb5c3
米亚 <futuretask@126.com>
2024-07-27 17:16:26 +0800

    add func

---
+++ backend-dao/src/main/java/com/langtuo/teamachine/dao/testor/SortTestor.java
@@ -0,0 +1,30 @@
+package com.langtuo.teamachine.dao.testor;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+
+public class SortTestor {
+    public static void main(String args[]) {
+        String result = formatGoodsCode("DFji-Abed1-2Jkl-0b2dfc-ajioKl", "-");
+        System.out.println(result);
+    }
+
+    public static String formatGoodsCode(String input, String separator) {
+        // 1. 使用separator分割字符串
+        String[] parts = input.split(String.valueOf(separator));
+
+        // 2. 对分割后的字符串数组进行字母升序排序
+        Arrays.sort(parts, String.CASE_INSENSITIVE_ORDER);
+
+        // 3. 使用newSeparator重新拼接排序后的字符串数组
+        StringBuffer result = new StringBuffer();
+        for (String str : parts) {
+            if (result.length() > 0) {
+                result.append(separator);
+            }
+            result.append(str);
+        }
+        return result.toString();
+    }
+}
