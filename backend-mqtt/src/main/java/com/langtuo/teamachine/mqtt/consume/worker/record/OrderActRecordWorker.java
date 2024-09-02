489c74ddf606e78bad47e15fb2e2e549c303719a
米亚 <futuretask@126.com>
2024-09-02 17:52:40 +0800

    add func

--- backend-mqtt/src/main/java/com/langtuo/teamachine/mqtt/consume/worker/record/OrderActRecordWorker.java
+++ backend-mqtt/src/main/java/com/langtuo/teamachine/mqtt/consume/worker/record/OrderActRecordWorker.java
@@ -17,54 +17,12 @@ import java.util.List;
 
 @Slf4j
 public class OrderActRecordWorker implements Runnable {
-    String msg = "{\n" +
-            "\t\"bizCode\": \"orderActRecord\",\n" +
-            "\t\"list\": [{\n" +
-            "  \t\"extraInfo\": {\n" +
-            "  \t\t\"abc\": \"def\"\n" +
-            "  \t},\n" +
-            "  \t\"idempotentMark\": \"1723700246767\",\n" +
-            "  \t\"machineCode\": \"abcd\",\n" +
-            "  \t\"orderGmtCreated\": 1723700246767,\n" +
-            "  \t\"outerOrderId\": \"111111\",\n" +
-            "  \t\"shopCode\": \"shop_001\",\n" +
-            "    \"shopName\": \"shop_001\",\n" +
-            "  \t\"shopGroupCode\": \"shopGroup_02\",\n" +
-            "    \"shopGroupName\": \"shopGroup_02\",\n" +
-            "  \t\"specItemList\": [{\n" +
-            "  \t\t\"specCode\": \"SPEC_SWEET\",\n" +
-            "  \t\t\"specItemCode\": \"SPEC_ITEM_7_SWEET\",\n" +
-            "  \t\t\"specItemName\": \"7分糖\",\n" +
-            "  \t\t\"specName\": \"甜度\"\n" +
-            "  \t}, {\n" +
-            "  \t\t\"specCode\": \"SPEC_BEIXING\",\n" +
-            "  \t\t\"specItemCode\": \"SPEC_ITEM_BIG\",\n" +
-            "  \t\t\"specItemName\": \"大杯\",\n" +
-            "  \t\t\"specName\": \"杯型\"\n" +
-            "  \t}],\n" +
-            "  \t\"state\": 0,\n" +
-            "  \t\"tenantCode\": \"tenant_001\",\n" +
-            "  \t\"toppingList\": [{\n" +
-            "  \t\t\"actualAmount\": 20,\n" +
-            "  \t\t\"stepIndex\": 1,\n" +
-            "  \t\t\"toppingCode\": \"topping_002\",\n" +
-            "  \t\t\"toppingName\": \"物料3\"\n" +
-            "  \t}, {\n" +
-            "  \t\t\"actualAmount\": 30,\n" +
-            "  \t\t\"stepIndex\": 1,\n" +
-            "  \t\t\"toppingCode\": \"topping_003\",\n" +
-            "        \"toppingName\": \"物料4\"\n" +
-            "  \t}]\n" +
-            "  }]\n" +
-            "}";
-
     /**
      *
      */
     private List<OrderActRecordPutRequest> requestList = Lists.newArrayList();
 
     public OrderActRecordWorker(JSONObject jsonPayload) {
-        jsonPayload = JSON.parseObject(msg);
         JSONArray jsonList = jsonPayload.getJSONArray(MqttConsts.RECEIVE_KEY_LIST);
         jsonList.forEach(jsonObject -> {
             OrderActRecordPutRequest request = TypeUtils.castToJavaBean(jsonObject, OrderActRecordPutRequest.class);
