597f03d399371bdaf96cc0525b132b798a02d0c0
米亚 <futuretask@126.com>
2024-10-01 22:34:27 +0800

    add func

--- backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineServletContextListener.java
+++ backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineServletContextListener.java
@@ -1,21 +1,43 @@
-//package com.langtuo.teamachine.web;
-//
-//import javax.servlet.ServletContextEvent;
-//import javax.servlet.ServletContextListener;
-//
-//public class TeaMachineServletContextListener implements ServletContextListener {
-//    @Override
-//    public void contextInitialized(ServletContextEvent sce) {
-//        // 应用启动时的操作
-//        System.out.println("teaMachineServletContextListener|contextInitialized|entering");
-//        System.out.println("teaMachineServletContextListener|contextInitialized|exiting");
-//    }
-//
-//    @Override
-//    public void contextDestroyed(ServletContextEvent sce) {
-//        // 应用停止时的操作
-//        // 例如: 关闭数据库连接，停止后台线程等
-//        System.out.println("$$$$$ ## teaMachineServletContextListener|contextDestroyed|entering");
-//        System.out.println("$$$$$ ## teaMachineServletContextListener|contextDestroyed|exiting");
-//    }
-//}
+package com.langtuo.teamachine.web;
+
+import com.langtuo.teamachine.biz.aync.threadpool.AsyncExeService;
+import com.langtuo.teamachine.mqtt.threadpool.ConsumeExeService;
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+public class TeaMachineServletContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        // 应用启动时的操作
+        System.out.println("teaMachineServletContextListener|contextInitialized|entering");
+        System.out.println("teaMachineServletContextListener|contextInitialized|exiting");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // 应用停止时的操作
+        // 例如: 关闭数据库连接，停止后台线程等
+        System.out.println("$$$$$ ## teaMachineServletContextListener|contextDestroyed|entering");
+
+        AsyncExeService.destroy();
+        ConsumeExeService.destroy();
+
+        AbandonedConnectionCleanupThread.uncheckedShutdown();
+        Enumeration<Driver> enumeration = DriverManager.getDrivers();
+        while (enumeration.hasMoreElements()) {
+            try {
+                DriverManager.deregisterDriver(enumeration.nextElement());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("$$$$$ ## teaMachineServletContextListener|contextDestroyed|exiting");
+    }
+}
--- backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineShutdownHook.java
+++ backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineShutdownHook.java
@@ -1,36 +1,14 @@
-package com.langtuo.teamachine.web;
-
-import com.langtuo.teamachine.biz.aync.threadpool.AsyncExeService;
-import com.langtuo.teamachine.mqtt.threadpool.ConsumeExeService;
-import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.stereotype.Component;
-
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Enumeration;
-
-@Component
-public class TeaMachineShutdownHook implements ApplicationListener<ContextClosedEvent> {
-    @Override
-    public void onApplicationEvent(ContextClosedEvent event) {
-        System.out.println("$$$$$ teaMachineShutdownHook|onApplicationEvent|entering");
-
-        AsyncExeService.destroy();
-        ConsumeExeService.destroy();
-
-        AbandonedConnectionCleanupThread.uncheckedShutdown();
-        Enumeration<Driver> enumeration = DriverManager.getDrivers();
-        while (enumeration.hasMoreElements()) {
-            try {
-                DriverManager.deregisterDriver(enumeration.nextElement());
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println("$$$$$ teaMachineShutdownHook|onApplicationEvent|exiting");
-    }
-}
+//package com.langtuo.teamachine.web;
+//
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.context.event.ContextClosedEvent;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class TeaMachineShutdownHook implements ApplicationListener<ContextClosedEvent> {
+//    @Override
+//    public void onApplicationEvent(ContextClosedEvent event) {
+//        System.out.println("$$$$$ teaMachineShutdownHook|onApplicationEvent|entering");
+//        System.out.println("$$$$$ teaMachineShutdownHook|onApplicationEvent|exiting");
+//    }
+//}
