b416b2ec82434ad68fec7602a52663cf558073dd
米亚 <futuretask@126.com>
2024-10-02 03:03:22 +0800

    add func

--- backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineDisposableBean.java
+++ backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineDisposableBean.java
@@ -1,13 +1,24 @@
-//package com.langtuo.teamachine.web;
-//
-//import org.springframework.beans.factory.DisposableBean;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class TeaMachineDisposableBean implements DisposableBean {
-//    @Override
-//    public void destroy() throws Exception {
-//        System.out.println("$$$$$ ## teaMachineDisposableBean|destroy|entering");
-//        System.out.println("$$$$$ ## teaMachineDisposableBean|destroy|exiting");
-//    }
-//}
+package com.langtuo.teamachine.web;
+
+import cn.hutool.extra.spring.SpringUtil;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TeaMachineDisposableBean implements DisposableBean {
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("$$$$$ ## teaMachineDisposableBean|destroy|entering");
+
+        HikariDataSource hikariDataSource = SpringUtil.getBean(HikariDataSource.class);
+        System.out.println("$$$$$ ## teaMachineDisposableBean|destroy|hikariDataSource|" + hikariDataSource);
+        if (hikariDataSource != null) {
+            hikariDataSource.getConnection().close();
+            hikariDataSource.close();
+        }
+        System.out.println("$$$$$ ## teaMachineDisposableBean|destroy|close|after");
+
+        System.out.println("$$$$$ ## teaMachineDisposableBean|destroy|exiting");
+    }
+}
