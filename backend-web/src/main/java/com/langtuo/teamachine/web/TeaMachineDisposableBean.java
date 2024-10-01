c638e5d6ad3123f3f92b186216e54ff8d37ba6a3
米亚 <futuretask@126.com>
2024-10-02 03:22:13 +0800

    add func

--- backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineDisposableBean.java
+++ backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineDisposableBean.java
@@ -5,11 +5,17 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.util.Map;
+
 @Component
 public class TeaMachineDisposableBean implements DisposableBean {
     @Override
     public void destroy() throws Exception {
         System.out.println("$$$$$ ## teaMachineDisposableBean|destroy|entering");
+
+        Map<String, DataSource> map = SpringUtil.getBeansOfType(DataSource.class);
+        System.out.println("$$$$$ ## teaMachineDisposableBean|destroy|dataSourceMap|" + map);
 
         HikariDataSource hikariDataSource = SpringUtil.getBean(HikariDataSource.class);
         System.out.println("$$$$$ ## teaMachineDisposableBean|destroy|hikariDataSource|" + hikariDataSource);
