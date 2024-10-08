7084ddb104b429e1809ba4f71af01854889a893b
米亚 <futuretask@126.com>
2024-10-08 08:03:35 +0800

    add func

--- backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineServletContextListener.java
+++ backend-web/src/main/java/com/langtuo/teamachine/web/TeaMachineServletContextListener.java
@@ -12,6 +12,12 @@ import java.sql.SQLException;
 import java.util.Enumeration;
 
 public class TeaMachineServletContextListener implements ServletContextListener {
+    /**
+     * 等待销毁的时间
+     */
+    private static final int WAITTING_DESTROY_TIME = 1000 * 5;
+
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         // 应用启动时的操作
@@ -25,9 +31,11 @@ public class TeaMachineServletContextListener implements ServletContextListener 
         // 例如: 关闭数据库连接，停止后台线程等
         System.out.println("## teaMachineServletContextListener|contextDestroyed|entering");
 
+        // 销毁线程池
         AsyncExeService.destroy();
         ConsumeExeService.destroy();
 
+        // 手动解除驱动注册
         Enumeration<Driver> enumeration = DriverManager.getDrivers();
         while (enumeration.hasMoreElements()) {
             try {
@@ -43,8 +51,9 @@ public class TeaMachineServletContextListener implements ServletContextListener 
             e.printStackTrace();
         }
 
+        // 等待清理完成
         try {
-            Thread.sleep(5000);
+            Thread.sleep(WAITTING_DESTROY_TIME);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
